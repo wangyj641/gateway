0e50cd35ab3df968517669516519178e0dd3229b
Yongjun <wangyj641@gmail.com>
2024-07-31 22:59:50 +0800

    Add user CURD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
+  imports: [UserModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,20 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
-describe('UserController', () => {
-  let controller: UserController;
+describe('UserService', () => {
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
       providers: [UserService],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
