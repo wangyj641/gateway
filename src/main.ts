0065de3e77327d05365c60c2d4cc7e37d6cba86b
Yongjun <wangyj641@gmail.com>
2024-08-01 16:45:00 +0800

    Add doc

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { generateDocument } from './doc';
 
 declare const module: any;
 
@@ -28,6 +29,9 @@ async function bootstrap() {
     defaultVersion: [VERSION_NEUTRAL, '1', '2'],
     type: VersioningType.URI,
   });
+
+  // Generate doc
+  generateDocument(app);
 
   // set hot module auto rebuild
   if (module.hot) {
