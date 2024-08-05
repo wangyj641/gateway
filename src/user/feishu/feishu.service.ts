3e7b1e5d9b23765e2e3fe58e57ba6df996263ab5
Yongjun <wangyj641@gmail.com>
2024-08-05 23:51:44 +0800

    Update comments

--- src/user/feishu/feishu.service.ts
+++ src/user/feishu/feishu.service.ts
@@ -27,7 +27,6 @@ export class FeishuService {
     if (!appToken) {
       const response = await getAppToken();
       if (response.code === 0) {
-        // token 有效期为 2 小时，在此期间调用该接口 token 不会改变。当 token 有效期小于 30 分的时候,再次请求获取 token 的时候，会生成一个新的 token，与此同时老的 token 依然有效。
         appToken = response.app_access_token;
         this.cacheManager.set(
           this.APP_TOKEN_CACHE_KEY,
@@ -35,7 +34,7 @@ export class FeishuService {
           response.expire - 60,
         );
       } else {
-        throw new BusinessException('飞书调用异常');
+        throw new BusinessException('feishu exception');
       }
     }
     return appToken;
