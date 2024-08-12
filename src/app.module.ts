9f545239b3117d2a4d2378c23480c3810250d68b
Yongjun <wangyj641@gmail.com>
2024-08-12 17:22:04 +0800

    Add redis as cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { getConfig } from './utils';
+import { CacheModule } from '@nestjs/cache-manager';
+//import * as redisStore from 'cache-manager-redis-store';
+import * as redisStore from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     CacheModule.register({
       isGlobal: true,
+
+      host: getConfig('REDIS_CONFIG').host,
+      port: getConfig('REDIS_CONFIG').port,
+      auth_pass: getConfig('REDIS_CONFIG').auth,
+      db: getConfig('REDIS_CONFIG').db,
     }),
     ConfigModule.forRoot({
       ignoreEnvFile: true,
@@ -18,7 +23,7 @@ import { getConfig } from './utils';
     }),
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
