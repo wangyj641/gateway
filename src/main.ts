3dd52c472fbf8af9268eeb8885aec5bfcd087751
Yongjun <wangyj641@gmail.com>
2024-08-13 17:21:14 +0800

    Add customized log

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,31 @@
-import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+import {
+  ValidationPipe,
+  VersioningType,
+  VERSION_NEUTRAL,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import fastify from 'fastify';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { FastifyLogger } from './common/logger';
 import { generateDocument } from './doc';
 
 declare const module: any;
 
 async function bootstrap() {
+  const fastifyInstance = fastify({
+    logger: FastifyLogger,
+  });
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter(fastifyInstance),
   );
 
   // set global interceptors
@@ -30,7 +40,9 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
 
-  // Generate doc
+  app.useGlobalPipes(new ValidationPipe());
+
+  // create document
   generateDocument(app);
 
   // set hot module auto rebuild
